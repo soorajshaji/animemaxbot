419fd037dbebe37d60cd79e4455367ac9b55e16a
soorajshaji <soorajshaji6@gmail.com>
2023-01-11 23:22:51 +0530

    update

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ const bot = new TelegramBot(token, { polling: true});
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   console.log(msg.text);
-  //bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, 'Received your message');
 });
