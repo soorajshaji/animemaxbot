d79768548e2cf22ded13d94ff2562b2648798e99
soorajshaji <soorajshaji6@gmail.com>
2023-01-11 23:06:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '5926132541:AAGH5bZtH1NNI9yCkKfx_yNU3Ei5PR7l-4k';
+
+const bot = new TelegramBot(token, { polling: true});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  console.log(msg.text);
+  //bot.sendMessage(chatId, 'Received your message');
+});
