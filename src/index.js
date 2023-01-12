922056211484abbea3805ec9338097bbcdc1da8c
soorajshaji <soorajshaji6@gmail.com>
2023-01-12 10:55:31 +0530

    working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '5926132541:AAGH5bZtH1NNI9yCkKfx_yNU3Ei5PR7l-4k';
+require('dotenv').config();
+
+const token = process.env.token;
 
 const bot = new TelegramBot(token, { polling: true});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   if (msg.text === '/start') {
-    bot.sendMessage(chatId, 'Hello');
+    bot.sendMessage(chatId, 'How you');
   }
+  bot.sendMessage(chatId, 'How you');
     
   
 
